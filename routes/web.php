f1cb1d780e7ed34b62a1280ebb3eb95c9f5fe702
rakesh3899 <rakeshmistry3899@gmail.com>
2019-09-09 16:47:58 +0600

    Define route group for frontend, backend as role

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,38 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+/***********************
+    route for backend
+ * ********************/
+Route::group(['as' => 'backend.', 'prefix' => 'backend', 'namespace' => 'Backend', 'middleware' => ['auth']], function (){
+    /***********************
+        route for admin
+     * ********************/
+    Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['admin']], function (){
+        //write here admin routes
+    });
+    /***********************
+        route for teacher
+     * ********************/
+    Route::group(['as' => 'teacher.', 'prefix' => 'teacher', 'namespace' => 'Teacher', 'middleware' => ['teacher']], function (){
+        //write here teacher routes
+    });
+    /***********************
+        route for student
+     * ********************/
+    Route::group(['as' => 'student.', 'prefix' => 'student', 'namespace' => 'Student', 'middleware' => ['student']], function (){
+        //write here student routes
+    });
+});
+/***********************
+    route for frontend
+ * ********************/
+Route::group(['as' => 'frontend.', 'namespace' => 'Frontend'], function (){
+    //write here frontend routes
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
