ec2dede2a9c66b06cb7173eb3cc1e2e9b0f787eb
rakesh3899 <rakeshmistry3899@gmail.com>
2019-09-10 17:55:24 +0600

    Role wise menubar separation

---
+++ resources/views/layouts/backend/menu/admin.blade.php
@@ -0,0 +1,88 @@
+<nav class="pcoded-navbar">
+    <div class="nav-list">
+        <div class="pcoded-inner-navbar main-menu">
+            <div class="pcoded-navigation-label">Navigation</div>
+            <ul class="pcoded-item pcoded-left-item">
+                <li class="active">
+                    <a href="#" class="waves-effect waves-dark">
+                        <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+                        <span class="pcoded-mtext">Dashboard</span>
+                    </a>
+                </li>
+                <li class="pcoded-hasmenu">
+                    <a href="javascript:void(0)" class="waves-effect waves-dark">
+                        <span class="pcoded-micon"><i class="feather icon-sidebar"></i></span>
+                        <span class="pcoded-mtext">Page layouts</span>
+                        <span class="pcoded-badge label label-warning">NEW</span>
+                    </a>
+                    <ul class="pcoded-submenu">
+                        <li class=" pcoded-hasmenu">
+                            <a href="javascript:void(0)" class="waves-effect waves-dark">
+                                <span class="pcoded-mtext">Vertical</span>
+                            </a>
+                            <ul class="pcoded-submenu">
+                                <li class="">
+                                    <a href="menu-static.html" class="waves-effect waves-dark">
+                                        <span class="pcoded-mtext">Static Layout</span>
+                                    </a>
+                                </li>
+                                <li class="">
+                                    <a href="menu-header-fixed.html" class="waves-effect waves-dark">
+                                        <span class="pcoded-mtext">Header Fixed</span>
+                                    </a>
+                                </li>
+                                <li class="">
+                                    <a href="menu-compact.html" class="waves-effect waves-dark">
+                                        <span class="pcoded-mtext">Compact</span>
+                                    </a>
+                                </li>
+                                <li class="">
+                                    <a href="menu-sidebar.html" class="waves-effect waves-dark">
+                                        <span class="pcoded-mtext">Sidebar Fixed</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="">
+                            <a href="menu-bottom.html" class="waves-effect waves-dark">
+                                <span class="pcoded-mtext">Bottom Menu</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="">
+                    <a href="navbar-light.html" class="waves-effect waves-dark">
+<span class="pcoded-micon">
+<i class="feather icon-menu"></i>
+</span>
+                        <span class="pcoded-mtext">Navigation</span>
+                    </a>
+                </li>
+
+            </ul>
+            <div class="pcoded-navigation-label">UI Element</div>
+            <ul class="pcoded-item pcoded-left-item">
+                <li class="pcoded-hasmenu">
+                    <a href="javascript:void(0)" class="waves-effect waves-dark">
+<span class="pcoded-micon">
+<i class="feather icon-box"></i>
+</span>
+                        <span class="pcoded-mtext">Basic</span>
+                    </a>
+                    <ul class="pcoded-submenu">
+                        <li class="">
+                            <a href="alert.html" class="waves-effect waves-dark">
+                                <span class="pcoded-mtext">Alert</span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </li>
+
+            </ul>
+
+
+        </div>
+    </div>
+</nav>
